e0e33e30150158f47d5ad0657b78013c66973e56
James Naadjie <james.naadjie@gmail.com>
2016-06-17 21:00:16 -0400

    initial commit

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I am not signed in$/) do
+	visit signup_path
+end
+
+When(/^I am on the signup page$/) do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see 'Sign Up'$/) do
+	pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/user_signup_feature.feature
@@ -0,0 +1,11 @@
+Feature: User Signup
+
+  So that I can user shopping cart
+  As a user
+  I want to sign up for an account
+
+  @wip
+  Scenario: User Signup
+	Given I am not signed in
+    When I am on the signup page
+    Then I should see 'Sign Up'
