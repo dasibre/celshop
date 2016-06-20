990158631014ba91d765eab53b1812adceddbe06
James Naadjie <james.naadjie@gmail.com>
2016-06-20 14:35:31 -0400

    refactor with current cart quantity helper

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
+
   def index
     @products = Product.all.sample(10)
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
 	def store_name
 		'Celmatix Shop'
 	end
+
+	def total_cart_items
+		current_order.item_quantity
+	end
+
 end
