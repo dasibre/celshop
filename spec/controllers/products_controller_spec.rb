1baa09517d1d82ba859cd851a0e16dbc24d283d1
James Naadjie <james.naadjie@gmail.com>
2016-06-20 14:45:08 -0400

    add pending clause

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ProductsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
+	    pending 'Not yet implemented'
       get :show
       expect(response).to have_http_status(:success)
     end
